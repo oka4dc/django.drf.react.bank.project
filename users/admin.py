22c3c5d75e41db625289e645accfaee6a6ef7926
Frederick Johnson <fritzj96@gmail.com>
2019-12-20 10:47:42 -0800

    added: User admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+from .models import User
 
 # Register your models here.
+@register(User)
+class UserAdmin(ModelAdmin):
+  pass

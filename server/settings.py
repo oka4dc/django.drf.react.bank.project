6047231920de98ce283aa93e67aee78caf137298
Frederick Johnson <fritzj96@gmail.com>
2019-12-20 10:05:41 -0800

    added: future staging host to ALLOWED_HOSTS

--- server/settings.py
+++ server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '']
+ALLOWED_HOSTS = ['localhost', 'staging-cohort-bank.herokuapp.com']
 
 
 # Application definition

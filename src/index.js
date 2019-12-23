bb713f21f2ee39b252c5d1ef570a1bc467408b27
Frederick Johnson <fritzj96@gmail.com>
2019-12-23 11:41:13 -0800

    fix: code formatter freaked out

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { App } from "./components";
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { App } from "./components";
 
 ReactDOM.render(<App />, document.getElementById("root"));

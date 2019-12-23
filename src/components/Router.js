f20aacdac5baef5f8ba8e44db3494df0a09fa098
Frederick Johnson <fritzj96@gmail.com>
2019-12-23 11:54:28 -0800

    added: basic router

---
+++ src/components/Router.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { IonReactRouter } from '@ionic/react-router';
+import { IonRouterOutlet } from '@ionic/react';
+
+export const Router = () => (
+  <IonReactRouter>
+    <IonRouterOutlet></IonRouterOutlet>
+  </IonReactRouter>
+);

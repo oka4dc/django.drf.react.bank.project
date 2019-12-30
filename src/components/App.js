1421b2edc7622cce5bff461e1d1dab4bf4bd6211
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 09:04:01 -0800

    added: IonApp

--- src/components/App.js
+++ src/components/App.js
@@ -3,5 +3,10 @@ import React from 'react';
 import { Router } from '.';
 
 import '@ionic/react/css/ionic.bundle.css';
+import { IonApp } from '@ionic/react';
 
-export const App = () => <Router />;
+export const App = () => (
+  <IonApp>
+    <Router />
+  </IonApp>
+);

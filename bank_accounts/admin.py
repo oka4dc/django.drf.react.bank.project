ad6cb7c3af38f4e33137aa5daef04bf4b26f996e
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 10:03:33 -0800

    added: admin for transactions & BankAccount

--- bank_accounts/admin.py
+++ bank_accounts/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.contrib.admin import register, ModelAdmin, TabularInline
+from .models import BankAccount, Transaction
+
+class TransactionInline(TabularInline):
+  model = Transaction
 
 # Register your models here.
+@register(BankAccount)
+class BankAccountAdmin(ModelAdmin):
+  inlines = [TransactionInline]

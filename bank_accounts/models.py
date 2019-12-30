fd23ecc55ce1de699dcef537dd8450787b24214b
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 09:59:01 -0800

    added: Transaction & BankAccount models

--- bank_accounts/models.py
+++ bank_accounts/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.conf import settings
+from django.db.models import Model, ForeignKey, CASCADE, CharField, IntegerField
 
 # Create your models here.
+
+account_types = (("CHK", "Checking"), ("SVG", "Savings"))
+
+class BankAccount(Model):
+  owner = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+  name = CharField(max_length=255)
+  type = CharField(max_length=3, choices=account_types)
+
+transaction_types = (("DEP", "Deposit"), ("WTH", "Withdrawal"))
+
+class Transaction(Model):
+  account = ForeignKey(BankAccount, on_delete=CASCADE)
+  type = CharField(max_length=3, choices=transaction_types)
+  amount = IntegerField()

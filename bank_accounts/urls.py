51df10b651fe411a0a4cf20085865969323cbb28
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 11:20:54 -0800

    added: setting up urls

--- bank_accounts/urls.py
+++ bank_accounts/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
+from .views import BankAccountList
 
+urlpatterns = [
+    path('', BankAccountList.as_view())
+]
--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('accounts/', include('bank_accounts.urls'))
 ]

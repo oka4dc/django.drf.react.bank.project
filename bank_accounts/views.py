f13308b939d59bcf2daf6689a1d751ffd9a3d58a
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 10:38:00 -0800

    added: ListCreateAPIView generic to views

--- bank_accounts/views.py
+++ bank_accounts/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 
 from .models import BankAccount
 from .serializers import BankAccountSerializer
 
-class BankAccountList(APIView):
-  def get(self, request, format=None):
-    bank_accounts = BankAccount.objects.all()
-    serializer = BankAccountSerializer(bank_accounts, many=True)
-    return Response(serializer.data)
+class BankAccountList(ListCreateAPIView):
+  queryset = BankAccount.objects.all()
+  serializer_class = BankAccountSerializer

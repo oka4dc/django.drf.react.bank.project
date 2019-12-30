308cd75b7370c758040e843d9f89389a1e4fa5cc
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 10:22:37 -0800

    added: BankAccountList View with get verb

--- bank_accounts/views.py
+++ bank_accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import BankAccount
+from .serializers import BankAccountSerializer
+
+class BankAccountList(APIView):
+  def get(self, request, format=None):
+    bank_accounts = BankAccount.objects.all()
+    serializer = BankAccountSerializer(bank_accounts, many=True)
+    return Response(serializer.data)

ecfde54bc63173ade27f1ad53431f1df837bf2e8
Frederick Johnson <fritzj96@gmail.com>
2019-12-30 11:17:26 -0800

    added: permission_classes to bank account list view

--- bank_accounts/views.py
+++ bank_accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .models import BankAccount
 from .serializers import BankAccountSerializer
@@ -8,6 +9,7 @@ from .serializers import BankAccountSerializer
 class BankAccountList(ListCreateAPIView):
   queryset = BankAccount.objects.all()
   serializer_class = BankAccountSerializer
+  permission_classes = [IsAuthenticated]
   
   def get_queryset(self):
     return BankAccount.objects.all().filter(owner=self.request.user)

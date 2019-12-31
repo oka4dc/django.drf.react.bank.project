5a2e58705ba7bd8dcb40e73bbf47b6214496340a
Frederick Johnson <fritzj96@gmail.com>
2019-12-31 11:18:30 -0800

    added: placeholder menu & splitpaine

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { IonReactRouter } from '@ionic/react-router';
-import { IonRouterOutlet } from '@ionic/react';
+import { IonRouterOutlet, IonMenu, IonSplitPane } from '@ionic/react';
 import { Route } from 'react-router-dom';
 
 import { LoginPage } from '../pages';
 
 export const Router = () => (
   <IonReactRouter>
-    <IonRouterOutlet>
-      <Route path="/" component={LoginPage} />
-    </IonRouterOutlet>
+    <IonSplitPane contentId="main">
+      <IonMenu />
+      <IonRouterOutlet id="main">
+        <Route path="/" component={LoginPage} />
+      </IonRouterOutlet>
+    </IonSplitPane>
   </IonReactRouter>
 );

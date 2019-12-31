504592cd39563b3b43fb2945147a7b1ccd57e23f
Frederick Johnson <fritzj96@gmail.com>
2019-12-31 11:16:58 -0800

    added LoginPage to router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { IonReactRouter } from '@ionic/react-router';
 import { IonRouterOutlet } from '@ionic/react';
+import { Route } from 'react-router-dom';
+
+import { LoginPage } from '../pages';
 
 export const Router = () => (
   <IonReactRouter>
-    <IonRouterOutlet></IonRouterOutlet>
+    <IonRouterOutlet>
+      <Route path="/" component={LoginPage} />
+    </IonRouterOutlet>
   </IonReactRouter>
 );

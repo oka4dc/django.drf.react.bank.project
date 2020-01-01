772fe129418e3be0a1e7306c53f75b3a50f5efe1
Frederick Johnson <fritzj96@gmail.com>
2020-01-01 09:58:32 -0800

    added: exports for account page

---
+++ src/pages/Account.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { IonPage } from '@ionic/react';
+
+export const AccountPage = () => {
+  return <IonPage></IonPage>;
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
 export { LoginPage } from './Login';
+export { AccountPage } from './Account';
